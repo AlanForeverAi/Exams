5275e3574ebe8421b6879bf3105d1bedfeb2683f
AlanForeverAi <alan19920626@gmail.com>
2014-10-12 10:02:46 +0800

    设置用户密码

--- server/LoginUI.cpp
+++ server/LoginUI.cpp
@@ -9,6 +9,8 @@ LoginUI::LoginUI(QWidget *parent) :
 void LoginUI::on_pushButton_login_clicked()
 {
             Manager m;
+            lineEdit_ID->setText("12345");
+            lineEdit_PW->setText("12345");
             m.setId(lineEdit_ID->text().toInt());
             m.setPassword(lineEdit_PW->text());
             emit this->loginSignal(m);
